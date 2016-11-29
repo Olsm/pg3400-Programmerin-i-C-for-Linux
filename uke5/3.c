12c6b1e580df8ab1594f9aa5c443f58780d06c1f
Olsm <OlavOlsm@gmail.com>
2016-11-29 19:07:09 +0100

    add solution week 5 exercise 3

--- uke5/3.c
+++ uke5/3.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#include <string.h>
+#include <limits.h>
 
 int main(void) {
+    char vowels[] = "aeiou";
+    char input[CHAR_MAX] = "";
 
+    scanf("%s", input);
+
+    puts("Occurences of vowels:");
+    for (int i = 0; i < strlen(input); i++) {
+        for (int j = 0; j < strlen(vowels); j++) {
+            if (input[i] == vowels[j]) {
+                printf("%c\n", input[i]);
+            }
+        }
+    }
 }
